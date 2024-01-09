bab4f4aec6b5e2048c147c5f8e73b966719f97c4
Nicky-16 <nickyv.s.ojeda@gmail.com>
2024-01-09 18:24:31 -0500

    primer cambio

---
+++ aplicando lo aprendido/assets/js/script.js
@@ -0,0 +1,15 @@
+//var numbers= [1,2,3,4];
+//numbers.push(5);
+//console.log(numbers);// [1,2,3,4,5]
+
+var parnumbers= [2,4,6,8];
+parnumbers.push(20);
+console.log(parnumbers);
+
+var frutas= ['manzana', 'platano', 'sandia', 'nispero'];
+frutas.push('kiwi');
+console.log(frutas);
+
+var meses= ['mayo', 'junio', 'julio', 'agosto'];
+meses.push('septiembre', 'HOLA');
+console.log(meses);
